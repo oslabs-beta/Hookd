da9b710936c6ffdd17bad9dd8ddad21dd8b9390c
Alexander <alxdip123@gmail.com>
2019-08-26 08:46:36 -0700

    working on state to hooks

--- util/useState.ts
+++ util/useState.ts
@@ -15,45 +15,64 @@ const App: string = fs.readFileSync(path.resolve(__dirname as string, '../static
 let ast: any = parse(App as string);
 
 // keeping global state for state to keep track of. can be placed in local scope to not have global var
-let state: any;
-const constructorVisitor = {
+const classVisitor = {
   ClassDeclaration(path: any){
     path.traverse({
+      //traverses into all the Class Methods
       ClassMethod(path: any){
-         path.traverse({
-          Identifier(path: any){
-            if(t.isIdentifier(path.node, {name: 'constructor'})){
-              path.parentPath.traverse({
-                AssignmentExpression(path: any){
-                  console.log('in AssignmentExpression')
-                  if (t.isExpression(path.node, {operator: '='})) {
-                    if(path.node.left.property.name === 'state') {
-                      console.log(path.node)
-                      state = path.node.right.properties;
-                    }
-                  }
+        // look specifically for the constructor method, where all the state is held
+        if(path.node.kind === 'constructor'){
+          let state;
+          // console.log('the path.node of ClassMethod: ', path.node)
+          path.traverse({
+            // since constructor exists, state or method bindings should exist(?)
+            AssignmentExpression(path: any){
+              // console.log('in AssignmentExpression')
+              if (t.isExpression(path.node, {operator: '='})) {
+                if(path.node.left.property.name === 'state') {
+                  // in an Assignment Exxpression, there will be a lift and a right
+                  // left will be what is being assigned (duh) and right will be the value(s) of what's being assigned
+                  
+                  // console.log(path.node)
+                  
+                  // we keep the value(s) in a variable. it will be an array.
+                  state = path.node.right.properties;
                 }
-              })
-              // this method will remove all of consructor() and leave a huge ass empty space
-              // find out how to remove all the white space and insert useState
-              // path.parentPath.remove()
-              makeUseStateNode(path as any, state as any)
-            } else {
-              // logic to traverse through other class methods go here
+              }
             }
-          }
-        })
+          })
+          // prepends 'const [state, setState] = useState(initVal)' outside of the constructor function
+          makeUseStateNode(path as any, state as any)
+        } else {
+          // logic to traverse through other class methods go here
+          path.traverse(memberExpVisitor)
+        }
+        // path.remove();
       }
     })
   }
 }
 
+// visitor to look for Member Expressions and replace this.setState and this.state with equivalent hooks
+const memberExpVisitor: object = {
+  MemberExpression(path: any){
+    if(path.node.property.name === 'setState'){
+      // console.log(path.node.property.name)
+      console.log(`yee i'm inside of member expression`)
+      setStateToHooks(path.parentPath)
+    } else if (path.node.property.name === 'state' && t.isThisExpression(path.node.object)){
+      console.log('gon change some state: ', path.node);
+      stateToHooks(path)
+    }
+  }
+}
+
+
 /**
- * this func will create 'const [state, setState] = useState(initState);' from 'rightObjProps' and insert as sibling from 'path'
+ * this func will create 'const [state, setState] = useState(initState);' from 'rightObjProps' and insert from 'path'
  * @param path the path to append siblings to before deletion
  * @param rightExpr the props array from ObjectExpression which contains the state
  */
-
 function makeUseStateNode(path: any, rightObjProps: any){
   // the rightObjProps will be an array
   for (let i = 0; i < rightObjProps.length; i++){
@@ -72,9 +91,41 @@ function makeUseStateNode(path: any, rightObjProps: any){
     // creates '[state, setState] = useState(initVal);'
     const varDecl = t.variableDeclarator(arrPatt, callExp);
     // adds 'const [state, setState] = useState(initState);' as a sibling
-    path.parentPath.insertBefore(t.variableDeclaration('const', [varDecl]))
+    path.insertBefore(t.variableDeclaration('const', [varDecl]))
   }
   // path.parentPath.remove();
+}
+
+/**
+ * replaces this.setState({ state: newState }) with setState(newState)
+ * ALERT -- place function within member expression visitor
+ * @param parentPath exactly what it says
+ */
+function setStateToHooks(parentPath: any) {
+  // this will be an array of arguments to make setState Call Arguments with
+  const args = parentPath.node.arguments[0].properties
+  const states = [];
+  for (let i = 0; i < args.length; i++){
+    // console.log(args[i]);
+  	// console.log(args[i].key.name);
+    const keyName = args[i].key.name;
+    const call = t.identifier('set' + upFirstChar(keyName))
+    const arg = args[i].value;
+    const callStatement = t.callExpression(call, [arg])
+    const expStatement = t.expressionStatement(callStatement)
+    states.push(expStatement)
+    // console.log(parentPath.parentPath.node);
+  }
+  parentPath.replaceWithMultiple(states)
+}
+
+/**
+ * 
+ * @param parentPath path.parentPath. this. what it says.
+ */
+function stateToHooks(path: any) {
+  console.log(path.node);
+  path.parentPath.remove();
 }
 
 /**
@@ -83,16 +134,15 @@ function makeUseStateNode(path: any, rightObjProps: any){
  * @returns a string
  * taken from a website do not steal
  */
-
 const upFirstChar = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
-
 
 traverse(ast , {
   enter(path: any) {
-    path.traverse(constructorVisitor);
+    path.traverse(classVisitor);
   }
 })
 const original = generate(ast as any);
+// console.log('this is what come sfrom log: ', log);
 
 fs.writeFileSync('test/ast.jsx', original.code as string);
 
