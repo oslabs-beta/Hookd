1f0c659a01240e9e525a01882fd77b728732399b
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2019-08-28 11:08:39 -0700

    useContext is ready to merge

--- util/useContext.ts
+++ util/useContext.ts
@@ -7,11 +7,8 @@ const fs: any = require('fs');
 const path: any = require('path');
 
 const file: string = fs.readFileSync(path.resolve((__dirname as string), '../static/dummyData/app.jsx')).toString();
-// console.log(file);
-const ast: object = parserMethods.parse(file); 
-// console.log('pre ast is', ast);
-// create functions in here and export in object
 
+const ast: object = parserMethods.parse(file); 
 
 interface Node {
   type: string;
@@ -51,12 +48,8 @@ const DeclarationStore: string[] = [];
 //create function to go through import statements and store in array, checking the Method Defenition 
 const ImpDeclVisitor: {ImportDeclaration: (path: Path) => void} = { 
   ImportDeclaration(path: Path): void {
-    // console.log('ImportDeclaration path is', path.node)
-    // console.log('Pre-traversal, DeclarationStore is' , DeclarationStore);
     // console.log('inside the import declaration, pushing those values into DeclStoreArray to check against the context consumers')
-    //go through import statements storing in DeclStore each identifier name['React','Component, UserWrapper, NameContext]
-
-    // console.log('path.node is ', path.node);
+    // console.log('ImportDeclaration path is', path.node)
     path.traverse({
       ImportDefaultSpecifier (path: Path): void {
         // console.log('inside path traversal of importDefaultSpecifier')
@@ -64,22 +57,12 @@ const ImpDeclVisitor: {ImportDeclaration: (path: Path) => void} = {
         // let array = path.node.local;
         // let value = path.node.local.name;
         DeclarationStore.push(path.node.local.name);
-        // console.log("DeclarationStore is", DeclarationStore )
-        // console.log('is path.node.local an array?:', Array.isArray(path.node.local));
-        // console.log('path.node.local looks like this:', path.node.local);
-        // console.log('array looks like this', array);
-        // console.log('value pulled from path.node.local is', value);
+      
       }
     })
   }
 }
    
-//why doesn't this one work????
-// const ProgramVisitor:  {Program: (path: Path) => void} = {
-//   Program(path: Path): void {
-//     console.log('we are at the program level')
-//   }
-// }
 
 
 let contextToUse: string = '';
@@ -95,23 +78,20 @@ const UseContextDecl: {ClassDeclaration: (path: Path) => void} = {
       JSXMemberExpression(path: Path): void {
         // console.log('inside the classMethod traversal stage')
         
-        //might not be necessary to check the right side could juest check the left, it's just one more level of nesting
-        //if right side of expression is "consumer"{
-          if(path.node.property.name.toLowerCase() === 'consumer'){
-            // console.group('match found');
-            // if DeclarationStore includes left side expession
-            if(DeclarationStore.includes(path.node.object.name)){
-              contextCount++;
-              contextToUse = path.node.object.name;
-              // console.log('context is found and contextToUse is', contextToUse);
-            }
+
+          //if right side of expression is "consumer", grab the value on the left side of the dot to constuct the useContext statement
+        if(path.node.property.name.toLowerCase() === 'consumer'){
+          // console.group('match found');
+          // if DeclarationStore includes left side expession
+          if(DeclarationStore.includes(path.node.object.name)){
+            contextCount++;
+            contextToUse = path.node.object.name;
+            // console.log('context is found and contextToUse is', contextToUse);
           }
-          // if the contextToUse we pulled out is in DeclarationStore
-          // enter into component, in component insertBefore or insertAfter
-          // unshift container, push container
-        }  
+        }
+      }  
     })
-    // console.log('contextCount is', contextCount);
+
     let i: number = 0;
     path.traverse({
       ClassMethod(path: Path): void {
@@ -141,8 +121,8 @@ const UseContextDecl: {ClassDeclaration: (path: Path) => void} = {
 
 ​
 
+// method for traversing through all the ImportSpecifiers, inserting hooks statements and taking out "component"
 const ImpSpecVisitor: {ImportSpecifier: (path: Path) => void} = {
-  // method for traversing through all the ImportSpecifiers
   ImportSpecifier(path: Path): void {
     // check to see if the property 'imported' is an identifier with the name 'Component'
     // console.log('inside the import specifier visitor function')
@@ -195,11 +175,6 @@ const ClassToFuncVisitor: {ClassDeclaration: (path: Path) => void} = {
       blockStatement = path.node.body.body;
       // console.log('possible blockStatement is', blockStatement)
       // const blockStatement = path.node.body.body;
-      // console.log('blockStatement is ', blockStatement)
-
-
-      // console.log('blockStatement is', blockStatement)
-      // const props: any [] = hasProps ? [t.identifier("props")] : [];
       path.traverse({
         ClassMethod(path: Path): void {
           // path.remove();
@@ -255,31 +230,5 @@ export {}
 
 
 
-//old func
-// const ClassToFuncVisitor: {ClassDeclaration: (path: Path) => void} = {
-//   ClassDeclaration(path: Path): void {
-//     const hasProps: boolean = false;
-
-//       //traverse to constructor(props) to see if props exist, if so flip the hasProps boolean to true
-//     if(path.get('params').isIdentifier({name: "props"})) { !hasProps };
-//     console.log('hasProps boolean should be true and right now it is', hasProps);
-    
-//     if(path.get('id').isIdentifier({ name: "App"})){
-//       const blockStatement = path.node.body.body;
-//       console.log('blockStatement is', blockStatement);
-//       // console.log('blockStatement is', blockStatement)
-//       const props: any [] = hasProps ? [t.identifier("props")] : [];
-      
-//       // path.replaceWith(
-//       //   t.variableDeclaration("const", 
-//       //   [t.variableDeclarator(
-//       //     t.identifier("App"), 
-//       //     t.arrowFunctionExpression(props, t.blockStatement(blockStatement) ) 
-//       //     )
-//       //   ])
-//       // )
-//     }  
-//   }
-// }
 
 
