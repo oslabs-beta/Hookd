779720af0eb214a2c29c27bc32c49cb77953cb1e
Alexander <alxdip123@gmail.com>
2019-09-03 16:15:53 -0700

    beginning of testing

---
+++ __test__/ClassToFunction.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Test1 extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+  render(){
+    return (
+      <div></div>
+    );
+  }
+}
+
+class Test2 extends Component {
+  render(){
+    return (
+      <p></p>
+    );
+  }
+}
