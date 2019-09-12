14afa5607b603c76c2aa165208c0d48ab036c65f
Alexander <alxdip123@gmail.com>
2019-09-11 23:18:52 -0700

    made separate packages for a cli and main module

--- index.ts
+++ index.ts
@@ -1,33 +1,17 @@
-// create functions here and export it in object
-
-import {parse,traverse,generate} from './util/constants/parser';
-import {ImpDeclVisitor, classDeclarationVisitor} from './util/constants/visitors';
+import { parse, traverse, generate } from './util/constants/parser';
+// if we can modularize visitors more, that'd be swell
+import { ImpDeclVisitor, classDeclarationVisitor } from './util/constants/visitors';
 
 // the main method to traverse the ast
-if (process.argv[2]) {
-  const fs = require('fs');
-  const path = require('path');
-  const file: string = fs.readFileSync(path.resolve((__dirname as string), process.argv[2]), 'utf-8').toString();
-  const ast: object = parse(file); 
-traverse(ast, {
-  enter(path: any) {
-    path.traverse(ImpDeclVisitor);
-    path.traverse(classDeclarationVisitor);
-  }
-})
-const newCode: string = generate(ast).code;
-if (!fs.existsSync(path.resolve(__dirname, './hookd'))) fs.mkdirSync(path.resolve(__dirname, 'hookd'));
-fs.writeFileSync(path.join(__dirname, `./hookd/${process.argv[2].split(/(\\|\/)/g).pop()}`), newCode as string)
-}
-function hookd (str: string): string {
+function hookd(str: string): string {
   const ast = parse(str);
   traverse(ast, {
-  enter(path: any) {
-    path.traverse(ImpDeclVisitor);
-    path.traverse(classDeclarationVisitor);
+    enter(path: any) {
+      path.traverse(ImpDeclVisitor);
+      path.traverse(classDeclarationVisitor);
     }
   });
   return generate(ast).code;
 }
 
-export { hookd };
+module.exports = hookd;
